751fcc6a753f891bb15f240a2b70478def191a12
Sharad singh Rajput <rsharad794@gmail.com>
2024-10-13 23:01:40 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended:true,limit:"16KB"}))
 
 app.use(express.static("public"))//to store images locally at public folder
 app.use(cookieParser())
+
+
+//routes
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
 export {app}
